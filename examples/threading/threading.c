04d4db3ea1a621f0f23155bae6fbd7d477e8ffbe
Anthony Zeater <anthony@resusright.com>
2023-03-01 11:17:55 +1100

    ASsignment 4 Part 1 Implementation

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -13,7 +13,31 @@ void* threadfunc(void* thread_param)
 
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
-    //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    
+    DEBUG_LOG("Sleeping for %d ms\n",thread_func_args->obtain_time_ms);
+
+    usleep(thread_func_args->obtain_time_ms*1000);
+
+    int rc = pthread_mutex_lock(thread_func_args->mutex);
+
+    if (rc != 0)
+    {
+        thread_func_args->thread_complete_success = false;
+	return thread_param;
+    }
+
+    usleep(thread_func_args->release_time_ms*1000);
+
+    rc = pthread_mutex_unlock(thread_func_args->mutex);
+    
+    if (rc != 0)
+    {
+        thread_func_args->thread_complete_success = false;
+        return thread_param;
+    }
+
+    thread_func_args->thread_complete_success = true;
     return thread_param;
 }
 
@@ -28,6 +52,28 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+    
+    struct thread_data * data = (struct thread_data*)malloc(sizeof(struct thread_data));
+
+    if (data == NULL)
+    {
+        ERROR_LOG("Could not allocate memory for data\n");
+	return false;
+    }
+
+    data->obtain_time_ms = wait_to_obtain_ms;
+    data->release_time_ms = wait_to_release_ms;
+    data->mutex = mutex;
+    data->thread_complete_success = false;
+
+    int rc = pthread_create(thread, NULL, threadfunc, data);
+
+    if (rc != 0)
+    {
+        ERROR_LOG("Could not start new thread\n");
+        return false;
+    }
+
+    return true;
 }
 
