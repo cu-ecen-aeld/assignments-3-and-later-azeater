7255c4309ac1b6952a99110f269177875e7e2847
Anthony Zeater <anthony@resusright.com>
2023-03-16 10:39:58 +1100

    Updated start script for different location

--- server/aesdsocket-start-stop.sh
+++ server/aesdsocket-start-stop.sh
@@ -1,17 +1,20 @@
 #! /bin/sh
 
 case "$1" in
-	start)
-		echo "Starting aesdsocket"
-		start-stop-daemon -S -n aesdsocket -a /bin/aesdsocket -- -d
-		;;
-	stop)
-		echo "Stopping aesdsocket"
-		start-stop-daemon -K -n aesdsocket
-		;;
-	*)
-		echo "Usage: $0 {start|stop}"
-	exit 1
+    start)
+        echo "Starting aesdsocket"
+        start-stop-daemon -S -n aesdsocket -a /bin/aesdsocket -- -d
+        if [ $? -ne 0 ]; then
+            start-stop-daemon -S -n aesdsocket -a usr/bin/aesdsocket -- -d
+        fi
+        ;;
+    stop)
+        echo "Stopping aesdsocket"
+        start-stop-daemon -K -n aesdsocket
+        ;;
+    *)
+        echo "Usage: $0 {start|stop}"
+    exit 1
 esac
 
 exit 0
